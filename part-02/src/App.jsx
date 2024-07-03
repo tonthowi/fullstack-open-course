e2eb0eb56903663b99182c6a51a59892060c1ab3
Tonthowi Al Ahyar <tonthowi@gmail.com>
2024-07-03 09:39:06 +0700

    Exercises 2.1.-2.5.

--- part-02/src/App.jsx
+++ part-02/src/App.jsx
@@ -1,61 +1,62 @@
-
-const Header = (props) => {
-  return (
-    <div>
-      <h1>{props.course}</h1>
-    </div>
-  )
-}
-
-const Part = (props) => {
-  return (
-    <div>
-      <p>{props.name} {props.exercises}</p>
-    </div>
-  )
-}
-
-const Content = (props) => {
-
-  return (
-    <div>
-      {props.parts.map((part) => {
-        return (
-          <Part key = {part.id} name = {part.name} exercises = {part.exercises} />
-        )
-      })}
-    </div>
-  )
-
-}
+import Courses from "./Courses"
 
 const App = () => {
-  const course = {
-    id: 1,
-    title: "Half Stack application development",
-    parts : [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+  // Define an array of courses
+  const courses = [
+    {
+      id: 1,
+      title: "Half Stack application development",
+      parts: [
+        {
+          name: "Fundamentals of React",
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: "Using props to pass data",
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: "State of a component",
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: "Redux",
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+
+    {
+      id: 2,
+      title: "Node JS",
+      parts: [
+        {
+          name: "Routing",
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: "Middlewares",
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: "GraphQL",
+          exercises: 10,
+          id: 3
+        }
+      ]
+    }
+  ]
 
   return (
     <>
-      <Header course = {course.title} />
-      <Content parts = {course.parts} />
+      {/* Render the Courses component and pass the courses array as a prop */}
+      <Courses courses={courses} />
     </>
   )
 }
